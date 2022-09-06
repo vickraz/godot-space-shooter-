421d612f07370c2734a44f859e3bd76bfe9bdb50
vickraz <victor.oberg@minervagymnasium.se>
2022-09-06 10:29:16 +0200

    Lektion 2: MainShip har skapats

---
+++ space shooter/Scripts/MainShip.gd
@@ -0,0 +1,42 @@
+extends KinematicBody2D
+
+const ACCELERATION = 1500
+
+var max_velocity := 500
+var direction := Vector2.RIGHT
+var velocity = Vector2.ZERO
+
+var can_boost := true
+
+func _ready() -> void:
+	#Startposition på skärmen
+	global_position = Vector2(300, 300)
+
+func _physics_process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("boost") and can_boost:
+		max_velocity = 800
+		can_boost = false
+		$BoostTimer.start()
+	
+	_move_ship(delta)
+	
+	print(max_velocity)
+	
+
+func _move_ship(delta: float) -> void:
+	if Input.is_action_pressed("move") and global_position.distance_to(get_global_mouse_position()) > 30:
+		var desired_velocity := direction.rotated(rotation) * max_velocity
+		velocity = velocity.move_toward(desired_velocity, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, ACCELERATION * delta)
+	
+	velocity = move_and_slide(velocity)
+	
+
+
+#Anropas automatiskt då timern tar slut
+func _on_BoostTimer_timeout() -> void:
+	can_boost = true
+	max_velocity = 500
--- space shooter/Scripts/SteeringShip.gd
+++ space shooter/Scripts/SteeringShip.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
-var speed = 400 
+const ACCELERATION = 1500
+var speed := 400 
+var velocity := Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
@@ -9,7 +11,9 @@ func _physics_process(delta: float) -> void:
 	#Direktionsvektorn kommer alltid att vara normaliserad dvs ha längden 1
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	var velocity = direction * speed
+	var desired_velocity := direction * speed
+	
+	velocity = velocity.move_toward(desired_velocity, ACCELERATION*delta)
 	
 	#KinematicBody2D har en inbyggd funktion som hanterar rörelse och 
 	#kollisioner i ett. Tiden delta används också automatiskt
