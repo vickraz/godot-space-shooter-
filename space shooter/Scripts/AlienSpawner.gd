983d5722814721fbdb22846dcf9322a00b6a70fe
vickraz <victor.oberg@minervagymnasium.se>
2022-10-27 09:08:47 +0200

    game over menu

--- space shooter/Scripts/AlienSpawner.gd
+++ space shooter/Scripts/AlienSpawner.gd
@@ -4,6 +4,9 @@ extends Node
 const WIDTH = 1024
 const HEIGHT = 600
 
+var spawn_freq: int = 9 #oftare ju lägre
+var number_of_spawns = 0
+
 var alien_scene = preload("res://Scenes/Alien.tscn")
 
 onready var player = get_parent().get_node("MainShip")
@@ -11,12 +14,21 @@ onready var HUD = get_parent().get_node("HUD")
 
 func _ready() -> void:
 	randomize()
-	$AlienSpawnTimer.wait_time = 3 + rand_range(-2, 2)
+	$AlienSpawnTimer.wait_time = rand_range(1, 1 + (spawn_freq % 10) / 10)
 	
 
 func _can_spawn_aliens() -> bool:
 	var number_of_aliens = len(get_tree().get_nodes_in_group("Enemy"))
 	return number_of_aliens <= 10
+
+func _set_spawn_freq() -> int:
+	if spawn_freq % 10 == 0:
+		if spawn_freq == 5:
+			return 5
+		else:
+			return spawn_freq - 1
+	else:
+		return spawn_freq
 
 func _spawn_alien() -> void:
 	var alien = alien_scene.instance()
@@ -53,10 +65,12 @@ func _spawn_swarm() -> void:
 	
 func _on_AlienSpawnTimer_timeout() -> void:
 	if _can_spawn_aliens():
-		var randomnumber = randi() % 10
+		number_of_spawns += 1
+		spawn_freq = _set_spawn_freq()
+		var randomnumber = randi() % spawn_freq
 		if randomnumber == 0:
 			_spawn_swarm()
 		else:
 			_spawn_alien()
-		$AlienSpawnTimer.wait_time = 3 + rand_range(-2, 2)
+		$AlienSpawnTimer.wait_time = rand_range(1, 1 + (spawn_freq % 10) / 10)
 		
